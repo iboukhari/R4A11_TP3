c5d17e967be160f6a7d923aa578c472008f5871c
Ilyas Boukhari <ilyasboukh87@gmail.com>
2025-02-05 17:14:04 +0100

    add TextField

--- app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
+++ app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
@@ -9,13 +9,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,6 +64,8 @@ fun AppNavigation(){
 
 @Composable
 fun FormScreen(navController: NavController) {
+    var name by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.Center,
@@ -65,6 +73,12 @@ fun FormScreen(navController: NavController) {
     ){
         Text(text = "Page du formulaire",
             style = MaterialTheme.typography.titleMedium)
+        TextField(
+            value = name,
+            onValueChange = { newText -> name = newText },
+            label = { Text("Entrez votre nom") },
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        )
         Spacer(modifier = Modifier.height(24.dp))
         Button(onClick = { navController.popBackStack() }){
             Text(text = "Retour")
