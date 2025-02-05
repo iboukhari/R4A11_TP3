5dd6acd753e505203eb4ca9b78e1e2a168284c73
Ilyas Boukhari <ilyasboukh87@gmail.com>
2025-02-05 17:02:44 +0100

    add HomeScreen and FormScreen

--- app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
+++ app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.tp2navigationcompose
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
 import com.example.tp2navigationcompose.ui.theme.TP2NavigationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +33,57 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TP2NavigationComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MaterialTheme{
+                    AppNavigation()
                 }
             }
+        }
+    }
+
+}
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home"){
+        composable("home"){
+            HomeScreen(navController = navController)
+        }
+        composable("form"){
+            FormScreen(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun FormScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Page du formulaire",
+            style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { navController.popBackStack() }){
+            Text(text = "Retour")
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Bienvenue dans ma première application compose navigation",
+            style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { navController.navigate("form") }){
+            Text(text = "Accéder au formulaire")
         }
     }
 }
